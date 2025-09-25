9adcfe4a5741909d6ba3e329a0bd4089c063784c
evankasten <evan.kasten@studentfr.ch>
2025-09-25 16:21:18 +0200

    exercie 3 fini

--- src/js/main.js
+++ src/js/main.js
@@ -37,6 +37,80 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   if (exercice3) {
-    // Ecrire ici tout le code pour l'exercice 3: Ecouteurs d'événement
+    const carte = document.querySelector("#exercice-3-1");
+    carte.addEventListener("click", function () {
+      carte.classList.add("highlight");
+    });
+    const carte2 = document.querySelector("#exercice-3-2");
+    const btn = carte2.querySelector("button");
+    btn.addEventListener("click", function () {
+      carte2.classList.toggle("highlight");
+    });
+
+    const carte3 = document.querySelector("#exercice-3-3");
+    carte3.addEventListener("mouseenter", function () {
+      carte3.classList.add("highlight");
+    });
+    carte3.addEventListener("mouseleave", function () {
+      carte3.classList.remove("highlight");
+    });
+    const carte4 = document.querySelector("#exercice-3-4");
+    carte4.addEventListener("mousemove", function () {
+      carte4.classList.add("highlight");
+    });
+    const carte5 = document.querySelector("#exercice-3-5");
+    window.addEventListener("scroll", function () {
+      carte5.classList.add("highlight");
+    });
+    const carte6 = document.querySelector("#exercice-3-6");
+    window.addEventListener("keydown", function () {
+      carte6.classList.add("highlight");
+    });
+    window.addEventListener("keyup", function () {
+      carte6.classList.remove("highlight");
+    });
+    const carte7 = document.querySelector("#exercice-3-7");
+    window.addEventListener("resize", function () {
+      carte7.classList.add("highlight");
+    });
+
+    // exercices ++
+    const carte8 = document.querySelector("#exercice-3-8");
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "f") {
+        carte8.classList.add("highlight");
+        console.log("Touche F pressée !");
+      }
+    });
+
+    const input = document.querySelector("#exercice-3-9 input");
+    const carte9 = document.querySelector("#exercice-3-9");
+    input.addEventListener("focus", function () {
+      carte9.classList.add("highlight");
+    });
+    input.addEventListener("blur", function () {
+      carte9.classList.remove("highlight");
+    });
   }
+  const input10 = document.querySelector("#exercice-3-10 input");
+  const carte10 = document.querySelector("#exercice-3-10");
+  input10.addEventListener("input", function () {
+    carte10.classList.add("highlight");
+  });
+
+  const input11 = document.querySelector("#exercice-3-11 input");
+  const carte11 = document.querySelector("#exercice-3-11");
+  input11.addEventListener("change", function () {
+    carte11.classList.add("highlight");
+  });
+
+  const input12 = document.querySelector("#exercice-3-12 input");
+  const carte12 = document.querySelector("#exercice-3-12");
+  input12.addEventListener("input", function () {
+    if (input12.value === "#ff0000") {
+      carte12.classList.add("highlight");
+    } else {
+      carte12.classList.remove("highlight");
+    }
+  });
 });
